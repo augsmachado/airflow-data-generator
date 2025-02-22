9b3899bbf04632ca804cbf1ef5ed2226d3612294
Augs Machado <augustomachadoramos@gmail.com>
2025-02-22 19:17:59 -0300

    Refactor dual_postgres_dag to implement SQLExecuteQueryOperator for the second PostgreSQL query and restore task execution order

--- dags/dual_postgres_dag.py
+++ dags/dual_postgres_dag.py
@@ -29,8 +29,6 @@ with DAG('dual_postgres_dag', default_args=default_args, schedule_interval=None)
         sql="SELECT NOW();"
     )
 
-
-    # Mensagem após a primeira query
     end_task1 = PythonOperator(
         task_id='end_query_postgres1',
         python_callable=log_message,
@@ -38,25 +36,23 @@ with DAG('dual_postgres_dag', default_args=default_args, schedule_interval=None)
     )
 
     # Mensagem antes da segunda query
-    # start_task2 = PythonOperator(
-    #     task_id='start_query_postgres2',
-    #     python_callable=log_message,
-    #     op_args=['Iniciando query no postgres2...']
-    # )
+    start_task2 = PythonOperator(
+        task_id='start_query_postgres2',
+        python_callable=log_message,
+        op_args=['Iniciando query no postgres2...']
+    )
 
-    # # Execução da query no segundo banco
-    # task2 = PostgresOperator(
-    #     task_id='query_postgres2',
-    #     postgres_conn_id='postgres2_conn',
-    #     sql="SELECT NOW();"
-    # )
+    task2 = SQLExecuteQueryOperator(
+        task_id='query_postgres2',
+        conn_id='postgres2_conn',
+        sql="SELECT NOW();"
+    )
 
-    # # Mensagem após a segunda query
-    # end_task2 = PythonOperator(
-    #     task_id='end_query_postgres2',
-    #     python_callable=log_message,
-    #     op_args=['Finalizando query no postgres2...']
-    # )
+    end_task2 = PythonOperator(
+        task_id='end_query_postgres2',
+        python_callable=log_message,
+        op_args=['Finalizando query no postgres2...']
+    )
 
     # Definição da ordem de execução
-    start_task1 >> task1 >> end_task1 #>> start_task2 >> task2 >> end_task2
+    start_task1 >> task1 >> end_task1 >> start_task2 >> task2 >> end_task2
